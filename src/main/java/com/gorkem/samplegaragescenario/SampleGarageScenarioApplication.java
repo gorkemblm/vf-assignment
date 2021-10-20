48c19c1e482e179c71d8c1b8d7cc986ed8acfc6e
Görkem Bilim <83100550+gorkemblm@users.noreply.github.com>
2021-10-20 14:25:34 +0300

    added-project

---
+++ src/main/java/com/gorkem/samplegaragescenario/SampleGarageScenarioApplication.java
@@ -0,0 +1,12 @@
+package com.gorkem.samplegaragescenario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGarageScenarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleGarageScenarioApplication.class, args);
+    }
+}
